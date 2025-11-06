caa45d0c8289735256e05df77bbb7094c5f93103
jediaelledenise <jediaelledenise.decastro@gmail.com>
2025-11-07 01:15:36 +0800

    Update logError.integration.test.js

--- tests/logError.integration.test.js
+++ tests/logError.integration.test.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import ErrorLog from '../models/errorLogs.js';
+import ErrorLog from '../src/models/errorLogs.js';
+import logError from '../logError.js';
 
 let mongod;
 beforeAll(async () => {
@@ -18,7 +19,7 @@ beforeEach(async () => {
 test('logError writes to ErrorLog collection', async () => {
   const fakeReq = { originalUrl: '/test', method: 'GET' };
   const err = new Error('Integration test error');
-  await logError(err, fakeReq);
+  await logError(err, fakeReq, ErrorLog); // <-- pass ErrorLog as third argument
   const doc = await ErrorLog.findOne({ message: 'Integration test error' });
   expect(doc).toBeTruthy();
   expect(doc.route).toBe('/test');
